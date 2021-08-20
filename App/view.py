4d047dbf8405439d786c72d4bdb1b7c1c8fa4860
Samuel Freire <s.freire@uniandes.edu.co>
2021-08-20 17:49:27 -0500

    cambios en main

--- App/view.py
+++ App/view.py
@@ -41,7 +41,7 @@ operación solicitada
 def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
-    print("2- Cargar Tags")
+    print("2- Cargar Tags!!!...")
     # TODO: Modificación de Est-1 en el Lab 2
     print("0- Salir")
 
